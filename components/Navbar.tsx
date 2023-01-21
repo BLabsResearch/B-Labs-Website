6d146132ffc6c6d69de9ee0d70c49edb6534058b
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-21 21:12:37 +0100

    suprememods

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,18 +30,26 @@ export default function Navbar() {
             <div className="bg-black w-64 h-screen fixed top-0 left-0 transition duration-200 ease-in-out transform -translate-x-full sidebar lg:hidden z-20">
                 <div className="p-3.5"><Image src={"/logo.png"} alt="logo" width={75} height={75} className="block lg:hidden" /></div>
                 <div className="px-3.5 space-y-0.5">
-                    <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
-                        <Link href="#about" className="text-[16px] text-white tracking-wide">About us</Link>
-                    </div>
-                    <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
-                        <Link href="#services" className="text-[16px] text-white tracking-wide">Services</Link>
-                    </div>
-                    <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
-                        <Link href="#strategies" className="text-[16px] text-white tracking-wide">Strategies</Link>
-                    </div>
-                    <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
-                        <Link href="#contact" className="text-[16px] text-white tracking-wide">Contact Us</Link>
-                    </div>
+                        <Link legacyBehavior href="#about">
+                            <div className="hover:cursor-pointer hover:bg-red-700 rounded-lg p-2.5 w-full">
+                                <p className="text-[16px] text-white tracking-wide w-full">About us</p>
+                            </div>
+                        </Link>
+                        <Link legacyBehavior href="#services">
+                            <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
+                                <p className="text-[16px] text-white tracking-wide w-full">Services</p>
+                            </div>
+                        </Link>
+                    <Link legacyBehavior href="#strategies">
+                        <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg text-[16px] text-white tracking-wide">
+                            <p className="text-[16px] text-white tracking-wide w-full">Strategies</p>
+                        </div>
+                    </Link>
+                    <Link legacyBehavior href="#contact">
+                        <div className="hover:cursor-pointer hover:bg-red-700 p-2.5 rounded-lg">
+                            <p className="text-[16px] text-white tracking-wide w-full">Contact Us</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
