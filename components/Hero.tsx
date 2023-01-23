f14909ea094fc98ebccdaf0bdb30141b80998cb2
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-24 00:53:36 +0100

    suprememods

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { SaveIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 import Button from "./Button";
 import Navbar from "./Navbar";
 
@@ -8,11 +9,11 @@ export default function Hero() {
             <Navbar />
             <div className="relative mt-20 sm:mt-24">
                 <div className="absolute w-full h-full top-[10%]">
-                    <div className="text-center space-y-5 text-xl">
-                        <h1 className="font-semibold text-white text-center text-2xl sm:text-3xl lg:text-5xl w-full">WE MAKE RESEARCH <span className="text-red-700">ACTIONABLE</span></h1>
+                    <div className="space-y-5 text-xl text-center">
+                        <h1 className="w-full text-2xl font-semibold text-center text-white sm:text-3xl lg:text-5xl">WE MAKE RESEARCH <span className="text-red-700">ACTIONABLE</span></h1>
                         <p className="text-white">by providing the highest quality research in web3</p>
-                        <div className="flex flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4 w-full justify-center items-center pt-5">
-                            <div>
+                        <div className="flex flex-col items-center justify-center w-full pt-5 space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+                            <Link href={"https://docs.google.com/presentation/d/1OyJqYT6xU3e1H4COrHvOaRe0MDXMvgca/edit#slide=id.p1"}>
                                 <button className="flex space-x-1 border-2 border-white p-2.5 rounded-lg items-center justify-center text-[14px] w-[182px]">
                                     <span>
                                         <SaveIcon className="w-6 h-6 text-white" />
@@ -21,7 +22,7 @@ export default function Hero() {
                                         OUR PITCH DECK
                                     </span>
                                 </button>
-                            </div>
+                            </Link>
                             <div>
                                 <Button text="WATCH OUR VIDEO" url="" />
                             </div>
