ca29ae2af708ddfbbcfc789e291c1870d1ace961
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-24 00:28:25 +0100

    suprememods

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,5 +1,15 @@
+import { CheckCircleIcon } from "@heroicons/react/outline";
+import Head from "next/head";
 export default function Thankyou() {
     return(
-        <div>Thank you</div>
+        <div className="w-full h-screen bg-black">
+            <Head>
+                <title>Thank you</title>
+            </Head>
+            <div className="flex flex-col items-center justify-center w-full h-full space-y-2.5">
+                <CheckCircleIcon className="w-40 h-40 text-red-700" />
+                <h3 className="font-semibold text-center text-white h3">Thank you for subscribing to our newsletter</h3>
+            </div>
+        </div>
     )
 }
