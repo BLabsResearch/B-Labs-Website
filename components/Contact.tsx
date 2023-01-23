7f50f5b685dc52c5ebc4f0ecb5bda4a05873b472
Supreme Labs <100731397+supreme2580@users.noreply.github.com>
2023-01-23 20:48:05 +0100

    suprememods

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
             <form className="max-w-2xl space-y-2.5">
                 <div className="w-full flex flex-col sm:flex-row sm:space-x-2.5 space-y-2.5 sm:space-y-0">
                     <div className="w-full flex-1">
-                        <p className="p text-white sm:h-auto">What should we call you?</p>
+                        <p className="p text-white sm:h-auto">Your name</p>
                         <input type="text" className="w-full p-2.5 border-2 border-red-700 bg-transparent rounded-md outline-none text-white" placeholder="Robin Smith" />
                     </div>
                     <div className="w-full flex-1">
@@ -34,15 +34,6 @@ export default function Contact() {
             <div className="w-full flex justify-center">
                 <button className="bg-red-700 text-white px-3.5 py-2.5 rounded-lg font-semibold text-[14px]">LET{"'"}S GO!</button>
             </div>
-<form action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post">
-    
-    email: <input type="text" name="email"/><br/>
-    <input type="hidden" name="campaign_token" value="fN4Li" />
-    
-    <input type="hidden" name="start_day" value="0" />
-    
-    <input type="submit" value="Subscribe"/>
-</form>
         </div>
     )
 }
