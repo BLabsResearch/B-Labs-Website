9879bf61cd5b3030f6d76d2e2b6a780eda9f5d45
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-29 14:00:56 +0100

    suprememods

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3J3RTR030Z%22%3E" strategy='afterInteractive' />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3J3RTR030Z');
+          `
+        }
+      </Script>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
